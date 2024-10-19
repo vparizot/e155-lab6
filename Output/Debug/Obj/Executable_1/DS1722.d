1e855f9c12f308c63ddb4ecd5e4adff7e6fbcef2
Victoria <vparizot@g.hmc.edu>
2024-10-19 14:43:58 -0700

    itit lab 6 repo

---
+++ Output/Debug/Obj/Executable_1/DS1722.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/Executable_1/DS1722.o: \
+ C:\Users\vparizot\E155\lab7_mvp\lib\DS1722.c
---
+++ Output/Debug/Obj/Executable_1/STM32L432KC_SPI.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/Executable_1/STM32L432KC_SPI.o: \
+ C:\Users\vparizot\E155\lab7_mvp\lib\STM32L432KC_SPI.c
